42ab1b962a3fab949491b6227508dae1ce35bfec
lebrisse <bricenwafo@yahoo.fr>
2014-10-17 10:36:26 +0100

    third commit

---
+++ src/main/java/com/adorsys/projet/login/RolePrincipal.java
@@ -0,0 +1,24 @@
+package com.adorsys.projet.login;
+
+	import java.security.Principal;
+
+	public class RolePrincipal implements Principal {
+	  
+	  private String name;
+	  
+	  public RolePrincipal(String name) {
+	    super();
+	    this.name = name;
+	  }
+
+	  public void setName(String name) {
+	    this.name = name;
+	  }
+
+	  @Override
+	  public String getName() {
+	  
+		  return name;
+	  }
+
+	}
